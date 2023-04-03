d6c8804a8ef5993356ffa53994d1c22c8d41be39
Emanuele Cappato <e.kappato@icloud.com>
2023-04-03 12:04:08 +0200

    Update articlegallerybtn.php

--- articlegallerybtn.php
+++ articlegallerybtn.php
@@ -7,6 +7,12 @@
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
  */
 
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Object\CMSObject;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Session\Session;
+
 defined('_JEXEC') or die;
 
 /**
@@ -14,7 +20,7 @@ defined('_JEXEC') or die;
  *
  * @since  1.5
  */
-class PlgButtonArticleGalleryBtn extends JPlugin
+class PlgButtonArticleGalleryBtn extends CMSPlugin
 {
 	/**
 	 * Load the language file on instantiation.
@@ -33,18 +39,24 @@ class PlgButtonArticleGalleryBtn extends JPlugin
 	 */
 	public function onDisplay($name)
 	{
-		$doc  = JFactory::getDocument();
+		//$doc  = JFactory::getDocument();
 
 		$link = '../plugins/editors-xtd/articlegallerybtn/display.php?ih_name='.$name;
 
-		JHTML::_('behavior.modal');
-		$button          = new JObject;
+		//JHTML::_('behavior.modal');
+		$button          = new CMSObject();
 		$button->modal   = true;
-		$button->class   = 'btn';
+		//$button->class   = 'btn';
 		$button->text    = JText::_('Article Gallery');
 		$button->name    = 'tag-2';
-		$button->options = "{handler: 'iframe', size: {x: 800, y: 500}}";
+		//$button->options = "{handler: 'iframe', size: {x: 800, y: 500}}";
 		$button->link    = $link;
+		$button->options = [
+                'height'     => '300px',
+                'width'      => '800px',
+                'bodyHeight' => '70',
+                'modalWidth' => '80',
+            	];
 
 		return $button;
 	}
