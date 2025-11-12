4ce75a89c3b51a24c3bd567d3b62e4b0e0d71dde
Emanuele Cappato <e.kappato@icloud.com>
2025-11-12 09:26:20 +0100

    Use Bootstrap 5 modal: load bootstrap.bundle and dynamic modal JS

--- media/plg_editor_ext_articleGalleryBtn/js/button.js
+++ media/plg_editor_ext_articleGalleryBtn/js/button.js
@@ -1,15 +1,29 @@
-// Minimal JS skeleton per Joomla 4: apri la gallery o una modal
-(function(window, document) {
-    'use strict';
+// JavaScript code handling the dynamic modal functionality
+function openArticleGallery() {
+    // Create a new Bootstrap modal
+    var modalHTML = `
+    <div class="modal fade" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Article Gallery</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <p>Content for the article gallery goes here...</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>`;
 
-    window.openArticleGallery = function() {
-        // TODO: sostituisci con la tua implementazione (apri una modal custom / view / chiamata API)
-        // Esempio minimo:
-        if (window.bootstrap && window.bootstrap.Modal) {
-            // Potresti creare una modal dinamica usando Bootstrap 5 se necessario
-            alert('Apri la gallery (sostituisci con la tua implementazione).');
-        } else {
-            alert('Apri la gallery (sostituisci con la tua implementazione).');
-        }
-    };
-})(window, document);
+    // Append modal HTML to the body
+    document.body.insertAdjacentHTML('beforeend', modalHTML);
+
+    // Show the modal
+    $('.modal').modal('show');
+}
--- plugins/editor/ext_articleGalleryBtn/plg_editor_ext_articleGalleryBtn.php
+++ plugins/editor/ext_articleGalleryBtn/plg_editor_ext_articleGalleryBtn.php
@@ -5,18 +5,8 @@ use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 
-/**
- * Plugin editor - ext_articleGalleryBtn (Joomla 4 compatible)
- */
 class PlgEditorExt_articleGalleryBtn extends CMSPlugin
 {
-    /**
-     * Application object (iniettato da CMSPlugin)
-     *
-     * @var \Joomla\CMS\Application\CMSApplication
-     */
-    protected $app;
-
     /**
      * Provide a button to the editor.
      *
@@ -29,29 +19,33 @@ class PlgEditorExt_articleGalleryBtn extends CMSPlugin
      */
     public function onDisplay($name, $asset, $author, $editor = null)
     {
-        // Carica le stringhe di lingua
+        // Load language strings
         $this->loadLanguage();
 
         $doc = Factory::getApplication()->getDocument();
 
-        // Joomla 4: usa WebAssetManager per registrare/caricare asset
         if (method_exists($doc, 'getWebAssetManager')) {
             $wa = $doc->getWebAssetManager();
-            // Registra e usa lo script (path relativo alla root del sito, media/...)
+
+            // Try to load Joomla's Bootstrap bundle (contains modal)
+            try {
+                $wa->useScript('bootstrap.bundle');
+            } catch (\Throwable $e) {
+                // If bootstrap.bundle is not registered we proceed anyway and load our script
+            }
+
+            // Register and use plugin script
             $wa->registerAndUseScript(
                 'plg.ext_articleGalleryBtn',
                 'media/plg_editor_ext_articleGalleryBtn/js/button.js',
                 [],
                 ['version' => 'auto']
             );
-            // Esempio per style:
-            // $wa->registerAndUseStyle('plg.ext_articleGalleryBtn.styles', 'media/plg_editor_ext_articleGalleryBtn/css/style.css');
         } else {
-            // Fallback (non comune in Joomla 4)
-            $doc->addScript(JURI::root(true) . '/media/plg_editor_ext_articleGalleryBtn/js/button.js');
+            // Fallback for older environments
+            $doc->addScript('/media/plg_editor_ext_articleGalleryBtn/js/button.js');
         }
 
-        // Costruisci l'oggetto bottone atteso dall'editor
         $button = new \stdClass;
         $button->modal = false;
         $button->class = 'btn btn-secondary';
